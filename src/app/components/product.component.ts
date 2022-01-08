3c877222f919de6620d6f63dfbf927d335c62a4a
slacksky <slacksky@gmail.com>
2022-01-08 17:53:11 -0500

    ciclo de vida y eventos de los componentes

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+     Component,
+     Input,
+     Output,
+     EventEmitter, 
+     OnChanges, 
+     SimpleChanges, 
+     OnInit, 
+     DoCheck, 
+     OnDestroy
+    } from '@angular/core';
 
 import { Product } from '../product.model';
 
@@ -7,9 +17,27 @@ import { Product } from '../product.model';
     templateUrl:'./product.component.html'
 })
 
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor(){
+        console.log('constructor');
+    }
+
+//    ngOnChanges(changes: SimpleChanges){
+//        console.log('ngOnchanges');
+//        console.log(changes);
+//    }
+    ngOnInit(){
+        console.log('ngOnInit');
+    }
+    ngDoCheck(){
+        console.log('ngDoCheck');//thisone might conflict with on changes
+    }
+    ngOnDestroy(){
+        console.log('ngOnDestroy')
+    }
 
     AddCart() {
         console.log('anadir al carrito');
