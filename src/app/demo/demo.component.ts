79fecdd33cd07c5afef498f139f37429bd6af885
slacksky <slacksky@gmail.com>
2022-02-07 16:47:30 -0500

    component partition and routing

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+  title = 'store';
+
+  items = ['Jorge', 'Anyela', 'Yasmin'];
+
+  power = 10;
+
+  
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  addItem(){
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index:number){
+    this.items.splice(index, 1);
+  }
+}
